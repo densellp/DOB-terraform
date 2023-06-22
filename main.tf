826ba0b63ca885c8052a72f22ea9dfee80d4c72a
densellp <densell321@gmail.com>
2023-06-22 14:20:17 -0700

    Fixed linting errors in main.tf

--- main.tf
+++ main.tf
@@ -25,11 +25,6 @@ resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = "westus2"
 
-  tags = {
-    Environment = "Terraform Getting Started"
-    Team = "DevOps"
-  }
-
 }
 
 # Create a virtual network
